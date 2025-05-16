b3669120653895ae1a5f8e406aeb82709d52a6e6
AndrosHomine13 <andres2005vargas@gmail.com>
2025-05-16 13:21:21 -0500

    Configura puerto dinámico para Render

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def admin():
     return render_template('admin.html', productos=productos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
