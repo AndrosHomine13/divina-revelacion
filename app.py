fb127db2c6530077abf4f26e52e3d7489811fdb0
AndrosHomine13 <andres2005vargas@gmail.com>
2025-05-18 22:17:10 -0500

    Actualizar app.py para que escuche en 0.0.0.0 y puerto dinámico

--- app.py
+++ app.py
@@ -89,8 +89,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
